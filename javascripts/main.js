e3b66045bbd7c7c10352ac25f39774c82796f406
sharon smith <sharon.e.smith2@gmail.com>
2017-05-09 19:49:16 -0500

    navbar angular controllers

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,13 @@
 
-console.log("in main javascripts");
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", ($scope) => {
+	$scope.cat = "Meow";
+	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+});
+
+
+app.controller("ItemCtrl", ($scope) => {
+	$scope.dog = "Woof!";
+
+});
