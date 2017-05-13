6e9a6fc7eed9eb9e08cad04300a174d61f80fdb1
sharon smith <sharon.e.smith2@gmail.com>
2017-05-13 11:43:07 -0500

    writing firebase items to DOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,6 @@
-
+app.run((FIREBASE_CONFIG) => {
+  firebase.initializeApp(FIREBASE_CONFIG);
+});
 
 
 app.controller("NavCtrl", ($scope) => {
@@ -7,7 +9,7 @@ app.controller("NavCtrl", ($scope) => {
 });
 
 
-app.controller("ItemCtrl", ($scope) => {
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 
 	$scope.dog = "Woof!";
 	$scope.showListView = true;
@@ -21,6 +23,37 @@ app.controller("ItemCtrl", ($scope) => {
 	$scope.allItems = () => {
 		$scope.showListView = true;
 	};
+
+
+  let getItemList = () => {
+    let itemz = [];
+    return $q((resolve, reject) => {
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+      .then((fbItems) => {
+        var itemCollection = fbItems.data;
+          Object.keys(itemCollection).forEach((key) => {
+            itemCollection[key].id=key;
+            itemz.push(itemCollection[key]);
+          });
+          resolve(itemz);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+  };
+
+
+  let getItems = () => {
+    getItemList().then((itemz) => {
+      $scope.items=itemz;
+    }).catch((error) => {
+      console.log("get Error", error);
+    });
+  };
+
+  getItems();
+
 
 
 });
