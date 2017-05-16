97e96026a3c69ad4941be87275e80a919d27ff97
sharon smith <sharon.e.smith2@gmail.com>
2017-05-15 21:13:49 -0500

    split out all the files

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,104 +1,12 @@
-app.run(function(FIREBASE_CONFIG) {
-  firebase.initializeApp(FIREBASE_CONFIG);
-});
-
-
-app.config(function($routeProvider) {
-  $routeProvider
-    .when('/items/list', {
-      templateUrl: 'partials/item-list.html',
-      controller: 'ItemListCtrl'
-    })
-    .when('/items/new', {
-      templateUrl: 'partials/item-new.html',
-      controller: 'ItemNewCtrl'
-    })
-    .when('/item/view/:id', {
-      templateUrl: 'partials/item-view.html',
-      controller: 'ItemViewCtrl'
-    })
-    .when('/item/edit/:id', {
-      templateUrl: 'partials/item-new.html',
-      controller: 'ItemEditCtrl'
-    })
-    .otherwise('items/list'); // default catch
-});
-
-
-app.controller("NavCtrl", function($scope) {
-	$scope.cat = "Meow";
-	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
-});
-
-
-app.controller("ItemListCtrl", function() {
-  console.log("inside ItemListCtrl");
-});
-
-
-app.controller("ItemNewCtrl", function() {
-  console.log("inside ItemNewCtrl");
-});
-
-
-app.controller("ItemViewCtrl", function() {
-  console.log("inside ItemViewCtrl");
-});
-
-
-app.controller("ItemEditCtrl", function() {
-  console.log("inside ItemEditCtrl");
-});
-
-
-
-
-
-
 app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 
-	$scope.dog = "Woof!";
-	$scope.showListView = true;
-	$scope.items = [];
+	
 
 
-	$scope.newItem = () => {
-		$scope.showListView = false;
-	};
-
-	$scope.allItems = () => {
-		$scope.showListView = true;
-	};
+  
 
 
-  let getItemList = () => {
-    let itemz = [];
-    return $q((resolve, reject) => {
-      $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
-      .then((fbItems) => {
-        var itemCollection = fbItems.data;
-          Object.keys(itemCollection).forEach((key) => {
-            itemCollection[key].id=key;
-            itemz.push(itemCollection[key]);
-          });
-          resolve(itemz);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-    });
-  };
-
-
-  let getItems = () => {
-    getItemList().then((itemz) => {
-      $scope.items=itemz;
-    }).catch((error) => {
-      console.log("get Error", error);
-    });
-  };
-
-  getItems();
+  
 
 
   let postNewItem = (newItem) => {
@@ -117,11 +25,8 @@ app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
   $scope.addNewItem = () => {
 
     $scope.newTask.isCompleted = false;
-// console.log("clicked add", $scope.newTask);
     postNewItem($scope.newTask).then((response) => {
       $scope.newTask = {};
-// console.log("id", response);
-      $scope.showListView = true;
       getItems();
     }).catch((error) => {
       console.log("Add error", error);
